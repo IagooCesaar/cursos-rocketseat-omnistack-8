9050c9e87e65b2f733647e0b64604e3eeac4fbb3
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-03-03 07:46:15 -0300

    Implementação de tratativa de login
    Vídeo aula 03, 0:49:04;
    Implementação de login e redirecionamento para página principal do frontend passando por parâmetro o ID do usuário logado

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Login.css';
 import logo from '../assets/logo.svg';
 
+import api from '../services/api';
 
-export default function Login() {
+export default function Login({history}) {
+    const [userName, setUserName] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault(); // prevenir do comportamento padrão do submit, que é trocar de pagina
+
+        console.log('Tentativa de login de usuário '+userName);
+        const response = await api.post('/devs', {
+            username: userName,
+        });          
+        console.log('Dados obtidos do usuário : '+userName,response);
+
+        const { _id } = response.data;
+        history.push(`/dev/${_id}`);
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev" />
                 <input 
                     placeholder="Digite seu usuário no Github"
+                    value={userName}
+                    onChange={e => setUserName(e.target.value)}
                 />
                 <button type="submit">Enviar</button>
             </form>
         </div>
     );
 }
+
