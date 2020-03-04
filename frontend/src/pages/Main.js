c8a7fc243fa514c5491b5f766d589f3b81fd23ad
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-03-04 07:53:44 -0300

    Construção da estrutura visual da página principal
    Vídeo aula 03, 0:58:58;
    Construção do layout da página principal com CSS

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import'./Main.css';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
-    return <h1>{match.params.id}</h1>
+    
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste </p>                    
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />                                
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like" />                                
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Teste</p>                    
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />                                
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like" />                                
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Teste</p>                    
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />                                
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like" />                                
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
+                    <footer>
+                        <strong>Filipe Deschamps</strong>
+                        <p>Teste</p>                    
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />                                
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like" />                                
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
 }
